9fdb974af27ee8b0a174d2751fa0cc30657ba122
salominreji0621 <salominrejir@gmail.com>
2026-01-06 12:13:43 +0530

    z pattern.c

---
+++ Z pattern .c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    int i, j, n = 5;
+
+    for(i = 1; i <= n; i++)
+    {
+        for(j = 1; j <= n; j++)
+        {
+            if(i == 1 || i == n || j == n - i + 1)
+                printf("* ");
+            else
+                printf("  ");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
